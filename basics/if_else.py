a7998a1ddb409e10da7cc725f4dbdecb576f9f20
prashanth-sams <sams.prashanth@gmail.com>
2020-07-05 18:44:00 +0400

    added loops and if else

---
+++ basics/if_else.py
@@ -0,0 +1,17 @@
+# one line if else statement
+print("true") if 1 == 1 else print('false')
+
+# one line for 3 conditions
+print("A") if 2 > 2 else print("=") if 2 == 2 else print("B")
+
+if 2 > 1 and 1 > 0:
+    pass
+
+if 2 > 1 or 1 < 0:
+    print('0 is smaller')
+
+if 2 > 1 > 0:
+    print('0 is smaller')
+
+if not 0 > 1 <= 2:
+    pass
