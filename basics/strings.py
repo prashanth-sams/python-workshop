4a4891b01ba9428655358e975bfe0136700fffd7
prashanth-sams <sams.prashanth@gmail.com>
2020-07-08 21:53:41 +0400

    strings

--- basics/strings.py
+++ basics/strings.py
@@ -29,3 +29,42 @@ print(name[0])
 # get char length
 print(len('am here'))
 print('am here'.__len__())
+
+k = " Jesus is coming soon"
+print('coming' in k)
+print('coming' not in k)
+print(k[1])
+print(k[2:5])
+print(k[-5:-1])
+print(k.strip())
+print(k.lower())
+print(k.upper())
+l = k.upper()
+print(l.isupper())
+m = k.lower()
+print(m.islower())
+m = k.replace('soon', 'soon.')
+print(m)
+print(m.split(' '))
+for i in k:
+    print(i)
+
+k = 'Hey! my name is {}'
+age = 30
+print(k.format(age))
+
+k = '{} is my name. I am {}'
+name = 'Prashanth'
+age = 30
+print(k.format(name, age))
+
+k = '{1} is my name. I am {0}'
+name = 'Prashanth'
+age = 30
+print(k.format(age, name))
+
+k = "Jesus is coming soon \\ \t \110\145\154\154\157 \x48\x65\x6c\x6c\x6f are you ready?\b?"
+print(k)
+
+k = 'jesus is coming soon'
+print(k.capitalize())
