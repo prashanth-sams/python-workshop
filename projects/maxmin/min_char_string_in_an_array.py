398f0c090298290d7233cc3c233b7fac8749c48c
prashanth-sams <sams.prashanth@gmail.com>
2021-03-09 00:00:54 +0400

    added projects

--- projects/maxmin/min_char_string_in_an_array.py
+++ projects/maxmin/min_char_string_in_an_array.py
@@ -1,9 +1,9 @@
 k = ['cat', 'zebra', 'girrafe', 'dog', 'tiger']
 
 for i in range(len(k)-1):
-    if len(k[i]) > len(k[i+1]):
+    if len(k[i]) < len(k[i+1]):
         temp = k[i]
         k[i] = k[i+1]
         k[i+1] = temp
 
-print(k[-1])
+print(k)
