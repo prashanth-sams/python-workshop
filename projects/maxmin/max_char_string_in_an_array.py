2fcd839eefef571a5627e4d30cf6749b5465ef5c
prashanth-sams <sams.prashanth@gmail.com>
2021-03-09 00:25:05 +0400

    added projects

--- projects/maxmin/max_char_string_in_an_array.py
+++ projects/maxmin/max_char_string_in_an_array.py
@@ -1,3 +1,4 @@
+# method 1
 k = ['cat', 'zebra', 'girrafe', 'dog', 'tiger']
 
 for i in range(len(k)-1):
@@ -7,3 +8,14 @@ for i in range(len(k)-1):
         k[i+1] = temp
 
 print(k[-1])
+
+# method 2
+k = ['cat', 'zebra', 'girrafe', 'dog', 'tiger']
+
+count = 0
+for i in range(len(k)-1):
+    if len(k[i]) > count:
+        count = len(k[i])
+        long = i
+
+print(k[long])
