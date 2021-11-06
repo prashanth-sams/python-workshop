052681547728aa53a045baf79f04ae350c785c9a
prashanth-sams <sams.prashanth@gmail.com>
2021-11-07 00:25:38 +0400

    added sub scripts

--- projects/prime_number.py
+++ projects/prime_number.py
@@ -31,3 +31,20 @@ if flag:
     print(num, "is not a prime number")
 else:
     print(num, "is a prime number")
+
+
+# method 3
+def checkPrimeNumber(value):
+    if(value == 1): return False
+    for i in range(2, value):
+        if(value % i == 0):
+            return False
+            break
+    
+    return True
+
+
+l = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+for i in l:
+    print(checkPrimeNumber(i))
--- projects/reverse/reverse_text.py
+++ projects/reverse/reverse_text.py
@@ -30,21 +30,28 @@ print(reverse('Hello world!'))
 
 
 # method 5
-k = 'Hello world!'
+k = "prashanth"
+l = ""
 
-output = []
+for i in k: l = i+l
 
-length = len(k)-1
-
-for i in range(len(k)):
-    while(length != -1):
-        output.append(k[length])
-        length -= 1
-
-print(''.join(output))
+print(l)
 
 
 # method 6
+k = "Hello world!"
+l = []
+
+length = len(k)-1
+
+while(length != -1):
+    l.append(k[length])
+    length -= 1
+
+print(''.join(l))
+
+
+# method 7
 k = 'Hello world!'
 
 output = []
@@ -53,3 +60,21 @@ for i in range(len(k)-1, -1, -1):
     output.append(k[i])
 
 print(''.join(output))
+
+
+# method 8
+k = 'Hello world!'
+l = ''
+
+for i in range(len(k)-1, -1, -1):
+    l = l + k[i]
+
+print(l)
+
+
+# method 9
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+print("".join(list(reverse('abcdefg'))))
