d07027992104801c7974e38422722a0467d6b2b7
prashanth-sams <sams.prashanth@gmail.com>
2022-11-18 01:19:19 +0400

    added type 3 reverse text alternate string

--- projects/reverse/reverse_text_alternate_remove.py
+++ projects/reverse/reverse_text_alternate_remove.py
@@ -17,3 +17,11 @@ print(name)
 # type #2
 x = 'Prashanth'[::-2]
 print(x)
+
+# type #3
+k = "prashanth"
+l = []
+
+for i, j in enumerate(range(len(k), 0, -1)):
+    if i%2==0:
+        print(k[j-1])
